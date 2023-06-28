7c2f3a977987bb784821feb799bb37fa23943384
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2023-06-28 17:24:56 +0900

    CORSでlocalhostの3000番を許可し、imagesを返すように

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import base64
 
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 # memo  uvicorn main:app --reload
 
 from models import stable_diffusion
@@ -9,8 +10,20 @@ from models import stable_diffusion
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 @app.get("/")
 async def root():
-    images = stable_diffusion.generate_image()
-    json_compatible_image_data = jsonable_encoder(images[0], custom_encoder={bytes: lambda v: base64.b64encode(v).decode('utf-8')})
-    return {"image_data": json_compatible_image_data}
+    images = stable_diffusion.generate_image(num_of_images=5)
+    # binaryデータをbase64に変換してレスポンス
+    return {"images": images}
